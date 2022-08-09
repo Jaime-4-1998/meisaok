e064d1b77d7dbc15f16f648bc93537f6aa7669a3
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2022-08-09 00:24:46 -0500

    Listo el de contacto con formu

--- Maniobra/index.php
+++ Maniobra/index.php
@@ -8,7 +8,6 @@
       <meta name="theme-color" content="#bf1520"/>
       <meta name="description" content="Meisa ofrece un amplio catálogo de productos de maquinaria, equipo y accesorios de cualquier ramo de la industria" />
       <title>Meisa - Maniobra</title>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
       <link rel="preload" href="../assets/css/style.css" as="style" onload="this.rel='stylesheet'">
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -102,6 +101,23 @@
     </section>
     <!--End-->
     <section>
+        <form class="form__data" id="form">
+            <label class="form__label">Nombre Completo</label>
+            <input class="form__control" type="text" name="" id="name" placeholder="Nombre Completo">
+            
+            <label class="form__label">Correo Electronico</label>
+            <input class="form__control" type="email" name="" id="mail" placeholder="corre@example.com">
+            
+            <label class="form__label">Teléfono</label>
+            <input class="form__control" type="text" name="" id="tel" placeholder="Telefono de contacto">
+            
+            <label class="form__label">Comentarios</label>
+            <textarea class="form__control" name="" id="tema" placeholder="Tema a tratar" rows="5" cols="12" ></textarea>
+            <small class="form__error ok fail" id="respuesta"></small>
+            <button id="submit" type="submit" class="buton__form">Enviar</button>
+        </form>
+    </section>
+    <section>
         <iframe title="Ubicación de Meisa MX" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3765.560208665229!2d-99.56701140850782!3d19.30148236724018!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd8abd8654e1a1%3A0xc0dbdbabbf1a84bf!2sSegunda%205%20de%20Mayo%2020%2C%20Reforma%2C%20Delegaci%C3%B3n%20Santa%20Mar%C3%ADa%20Totoltepec%2C%2052100%20Toluca%20de%20Lerdo%2C%20M%C3%A9x.!5e0!3m2!1ses!2smx!4v1658210888350!5m2!1ses!2smx" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
     <footer>
@@ -110,7 +126,6 @@
         ?>
     </footer>
     <script src="../assets/js/index.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-    <script src="../assets/js/carru.js"></script>
+    <script src="../assets/js/whp.js"></script>
 </body>
 </html>
