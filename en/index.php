23837430c42602ac02642d5ddc2bf43d9c80587f
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2022-08-09 21:37:04 -0500

    listo el ingles

--- en/index.php
+++ en/index.php
@@ -9,7 +9,7 @@
       <meta name="description" content="Meisa ofrece un amplio catálogo de productos de maquinaria, equipo y accesorios de cualquier ramo de la industria" />
       <title>Meisa</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
-      <link rel="preload" href="assets/css/style.css" as="style" onload="this.rel='stylesheet'">
+      <link rel="preload" href="../assets/css/style.css" as="style" onload="this.rel='stylesheet'">
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" as="style" onload="this.rel='stylesheet'"> 
@@ -19,7 +19,7 @@
          <div class="header">
             <div class="first__column__head">
                <div class="img__head">
-                  <img src="assets/img/lg/9800mei039087053.svg" loading="lazy" alt="Meisa MX" title="Meisa MX" width="50" height="100"/>
+                  <img src="../assets/img/lg/9800mei039087053.svg" loading="lazy" alt="Meisa MX" title="Meisa MX" width="50" height="100"/>
                </div>
             </div>
             <div class="second__column__head">
@@ -39,17 +39,24 @@
     </header>
     <nav>
         <?php
-            include 'assets/components/nav.php';
+            include '../assets/components/naveng.php';
         ?>
     </nav>
     <section>
          <a href="https://api.whatsapp.com/send?phone=5215535688727&text=Hola!&nbsp;me&nbsp;pueden&nbsp;apoyar?" target="_blank">
-         <img src="assets/img/art/whatsappsvg.svg" loading="lazy" alt="Contacto de Meisa MX" title="Contacto de Meisa MX" class="icon__wh" width="4" height="100" />
+         <img src="../assets/img/art/whatsappsvg.svg" loading="lazy" alt="Contacto de Meisa MX" title="Contacto de Meisa MX" class="icon__wh" width="4" height="100" />
          </a>
     </section>
-    <!--Carrusel-->
-    <?php include_once "assets/func/func.php"; ?>
-    <section>
+     <!--Carrusel-->
+     <?php 
+        function obtenerBanners(){
+            include '../meisaback/adminmeisa/assets/components/backend/conexion.php';
+            $consulta = $mbd->prepare("SELECT * FROM banners ORDER BY prioridad ASC");
+            $consulta->execute();
+            return $consulta->fetchAll();
+        }
+     ?>
+     <section>
         <!-- Swiper -->
         <div class="swiper mySwiper">
             <div class="swiper-wrapper">
@@ -60,13 +67,13 @@
                             if($i === 0){
                     ?>
                         <div class="swiper-slide">
-                            <img src="meisaback/adminmeisa/assets/<?php echo $banerresult['img'] ?>" loading="lazy" title="<?php echo $banerresult['seo']?>" alt="<?php echo $banerresult['seo']?>" width="100" height="100" />
+                            <img src="../meisaback/adminmeisa/assets/<?php echo $banerresult['img'] ?>" loading="lazy" title="<?php echo $banerresult['seo']?>" alt="<?php echo $banerresult['seo']?>" width="100" height="100" />
                         </div>
                     <?php
                         }else{
                     ?>
                         <div class="swiper-slide">
-                            <img src="meisaback/adminmeisa/assets/<?php echo $banerresult['img'] ?>" loading="lazy" title="<?php echo $banerresult['seo']?>" alt="<?php echo $banerresult['seo']?>" width="100" height="100" />
+                            <img src="../meisaback/adminmeisa/assets/<?php echo $banerresult['img'] ?>" loading="lazy" title="<?php echo $banerresult['seo']?>" alt="<?php echo $banerresult['seo']?>" width="100" height="100" />
                         </div>
                     <?php
                         }        
@@ -78,14 +85,14 @@
             <div class="swiper-button-prev"></div>
             <div class="swiper-pagination"></div>
         </div>
-    </section>
+     </section>
     <!--End-->
-    <!--Content-->
-    <section>
+     <!--Content-->
+     <section>
          <div class="content__homme">
             <?php
-               include 'meisaback/adminmeisa/assets/components/backend/conexion.php';
-               $sql = "SELECT id_home,title_one,desc_one
+               include '../meisaback/adminmeisa/assets/components/backend/conexion.php';
+               $sql = "SELECT id_home,title_eng,desc_oneng
                FROM home ORDER BY id_home ASC"; 
                $query = $mbd -> prepare($sql); 
                $query -> execute(); 
@@ -93,17 +100,17 @@
                    if($query -> rowCount() > 0){ 
                        foreach($results as $result) { 
                            $data = $result -> id_home."||".
-                           $result -> title_one."||".
-                           $result -> desc_one; 
+                           $result -> title_eng."||".
+                           $result -> desc_oneng; 
             ?>
             <h1>
                <div class="title__homme">
-                  <?php echo $result -> title_one; ?>  
+                  <?php echo $result -> title_eng; ?>  
                </div>
             </h1>
             
             <div class="parr__homme">
-               <?php echo $result -> desc_one; ?>
+               <?php echo $result -> desc_oneng; ?>
             </div>
             
             <?php
@@ -111,8 +118,8 @@
                }
             ?>
             <?php
-               include 'meisaback/adminmeisa/assets/components/backend/conexion.php';
-               $sql = "SELECT id_home,titlesec,editorsecond
+               include '../meisaback/adminmeisa/assets/components/backend/conexion.php';
+               $sql = "SELECT id_home,titleseceng,editorsecondeng
                FROM hometwo ORDER BY id_home ASC"; 
                $query = $mbd -> prepare($sql); 
                $query -> execute(); 
@@ -120,17 +127,17 @@
                    if($query -> rowCount() > 0){ 
                        foreach($results as $result) { 
                            $data = $result -> id_home."||".
-                           $result -> titlesec."||".
-                           $result -> editorsecond; 
+                           $result -> titleseceng."||".
+                           $result -> editorsecondeng; 
                        ?>
             <h2>
                <div class="title__homme">
-                  <?php echo $result -> titlesec; ?>  
+                  <?php echo $result -> titleseceng; ?>  
                </div>
             </h2>
            
             <div class="parr__homme">
-               <?php echo $result -> editorsecond; ?>
+               <?php echo $result -> editorsecondeng; ?>
             </div>
             
             <?php
@@ -138,56 +145,56 @@
                }
                ?>
          </div>
-    </section>
-    <!--End-->
-    <!--Columns-->
-    <div class="grid">
+     </section>
+     <!--End-->
+     <!--Columns-->
+     <div class="grid">
+                <?php
+                    include '../meisaback/adminmeisa/assets/components/backend/conexion.php';
+                    $sql = "SELECT id_col,prioridad,img,engtitle,engcont
+                    FROM columnfour ORDER BY prioridad ASC"; 
+                    $query = $mbd -> prepare($sql); 
+                    $query -> execute(); 
+                    $results = $query -> fetchAll(PDO::FETCH_OBJ); 
+                        if($query -> rowCount() > 0){ 
+                            foreach($results as $result) { 
+                                $data = $result -> id_col."||".
+                                $result -> prioridad."||".
+                                $result -> img."||".
+                                $result -> engtitle."||".
+                                $result -> engcont; 
+                ?>
+            <div class="flex__col">
+                <h2>
+                    <div class="flex__title">
+                        <?php echo $result -> engtitle; ?>
+                    </div>
+                </h2>
+                <img class="imgh" loading="lazy" src="../meisaback/adminmeisa/assets/<?php echo $result -> img; ?>" alt="<?php echo $result -> engtitle; ?>" title="<?php echo $result -> engtitle; ?>" width="35" height="35" />
+                
+                    <div class="flex__parr">
+                        
+                        <?php echo $result -> engcont; ?>
+                        
+                    </div>
+                
+            </div>
             <?php
-                include 'meisaback/adminmeisa/assets/components/backend/conexion.php';
-                $sql = "SELECT id_col,prioridad,img,title,content
-                FROM columnfour ORDER BY prioridad ASC"; 
-                $query = $mbd -> prepare($sql); 
-                $query -> execute(); 
-                $results = $query -> fetchAll(PDO::FETCH_OBJ); 
-                    if($query -> rowCount() > 0){ 
-                        foreach($results as $result) { 
-                            $data = $result -> id_col."||".
-                            $result -> prioridad."||".
-                            $result -> img."||".
-                            $result -> title."||".
-                            $result -> content; 
-            ?>
-         <div class="flex__col">
-            <h2>
-                <div class="flex__title">
-                    <?php echo $result -> title; ?>
-                </div>
-            </h2>
-            <img class="imgh" loading="lazy" src="meisaback/adminmeisa/assets/<?php echo $result -> img; ?>" alt="<?php echo $result -> title; ?>" title="<?php echo $result -> title; ?>" width="35" height="35" />
-            
-                <div class="flex__parr">
-                    
-                    <?php echo $result -> content; ?>
-                    
-                </div>
-            
-         </div>
-         <?php
-            }
-            }
-            ?>
-    </div>
-    <!--End-->
+                }
+                }
+                ?>
+     </div>
+     <!--End-->
     <section>
         <iframe title="Ubicación de Meisa MX" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3765.560208665229!2d-99.56701140850782!3d19.30148236724018!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd8abd8654e1a1%3A0xc0dbdbabbf1a84bf!2sSegunda%205%20de%20Mayo%2020%2C%20Reforma%2C%20Delegaci%C3%B3n%20Santa%20Mar%C3%ADa%20Totoltepec%2C%2052100%20Toluca%20de%20Lerdo%2C%20M%C3%A9x.!5e0!3m2!1ses!2smx!4v1658210888350!5m2!1ses!2smx" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
     <footer>
         <?php
-            include 'assets/components/footer.php';
+            include '../assets/components/footer.php';
         ?>
     </footer>
-    <script src="assets/js/index.js"></script>
+    <script src="../assets/js/index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-    <script src="assets/js/carru.js"></script>
+    <script src="../assets/js/carru.js"></script>
 </body>
 </html>
