3dc961ae32993befb531f7dfb4d857e2d33cc305
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2022-08-10 23:27:59 -0500

    Cambios del Footer

--- assets/components/footer.php
+++ assets/components/footer.php
@@ -1,6 +1,6 @@
 <div class="footer__first">
    <div class="footer__hr">
-      <img src="https://meisamex.com.mx/assets/img/art/cloc.svg" loading="lazy" alt="Horarios de Meisa MX" title="Horarios de Meisa MX" width="28" height="100">
+      <img src="https://meisamex.com.mx/assets/img/art/clocnew.svg" loading="lazy" alt="Horarios de Meisa MX" title="Horarios de Meisa MX" width="28" height="100">
       <p>Lunes a viernes de 9:00am - 18:00pm</p>
    </div>
    <p>Sábados de 9:00am - 14:00pm</p>
@@ -13,21 +13,21 @@
 <div class="footer__four">
    <div class="footer__social__line movil__footer">
       <a href="https://www.facebook.com/meisaequip" title="Facebook" target="_blank">
-      <img src="https://meisamex.com.mx/assets/img/social/facebook.svg" loading="lazy" alt="Facebook @meisaequip" title="Facebook @meisaequip" width="6" height="6" /> @meisaequip
+      <img src="https://meisamex.com.mx/assets/img/social/lg_fb.svg" loading="lazy" alt="Facebook @meisaequip" title="Facebook @meisaequip" widht="100" height="100"  />
       </a>
       <a href="https://www.instagram.com/meisa_mex/" title="Instagram" target="_blank">
-      <img src="https://meisamex.com.mx/assets/img/social/insta.svg" loading="lazy" alt="Instagram @meisa_mex" title="Instagram @meisa_mex" width="6" height="6" /> @meisa_mex
+      <img src="https://meisamex.com.mx/assets/img/social/lg_ig.svg" loading="lazy" alt="Instagram @meisa_mex" title="Instagram @meisa_mex" widht="100" height="100"  />
       </a>
    </div>
-   <div class="footer__social__line">
+   <div class="footer__social__line movil__footer">
       <a href="https://twitter.com/meisamex" title="Twitter" target="_blank">
-      <img src="https://meisamex.com.mx/assets/img/social/tw.svg" loading="lazy" class="social__media__img" alt="Twitter @meisa_mex" title="Twitter @meisa_mex" width="6" height="6" /> @meisamex
+      <img src="https://meisamex.com.mx/assets/img/social/lg_tw.svg" loading="lazy"  alt="Twitter @meisa_mex" title="Twitter @meisa_mex" widht="100" height="100"  />
       </a>
       <a href="https://www.youtube.com/user/meisaequipos" title="Youtube" target="_blank">
-      <img src="https://meisamex.com.mx/assets/img/social/you.svg" loading="lazy" class="social__media__img" alt="Youtube @meisa_mex" title="Youtube @meisa_mex" width="6" height="6" /> Meisa Mex
+      <img src="https://meisamex.com.mx/assets/img/social/lg_you.svg" loading="lazy"  alt="Youtube @meisa_mex" title="Youtube @meisa_mex" widht="100" height="100"  />
       </a>
       <a href="https://www.linkedin.com/in/meisamex/" title="Linkedin" target="_blank">
-      <img src="https://meisamex.com.mx/assets/img/social/lin.svg" loading="lazy" class="social__media__img" alt="Linkedin @meisa_mex" title="Linkedin @meisa_mex" width="6" height="6" /> @meisamex
+      <img src="https://meisamex.com.mx/assets/img/social/lg_lin.svg" loading="lazy"  alt="Linkedin @meisa_mex" title="Linkedin @meisa_mex" widht="100" height="100"  />
       </a>
    </div>
 </div>
