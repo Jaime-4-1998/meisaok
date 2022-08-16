eda71e801b2d0e56b5050ff19dcffb69d6a454cb
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2022-08-15 22:46:17 -0500

    Listo la parte de inlges

---
+++ assets/components/footeren.php
@@ -0,0 +1,42 @@
+<div class="li__footer">
+         <div class="logo__foter">
+            <img src="https://meisamex.com.mx/assets/img/lg/9800mei039087053.svg" alt="Meisa MEX" title="Meisa MEX" width="80" height="100" />
+         </div>
+         <p>
+         <img src="https://meisamex.com.mx/assets/img/art/icon-location.svg" alt="">Segunda 5 de Mayo 20
+            Reforma, Delegación Santa María Totoltepec
+            52100 Toluca de Lerdo, Méx. 
+         </p>
+         <a href="tel:+527222117664"> (+52) 722-211-7694</a>
+         <a href="tel:+5215535688727"> (+521) 55-3568-8727</a>
+         <a href="mailto:contacto@meisamex.com.mx">contacto@meisamex.com.mx</a>
+         <div class="footer__social">
+            <a href="https://www.facebook.com/meisaequip" target="_blank" rel="noopener" aria-label="Facebook" title="Facebook">
+             <img src="https://meisamex.com.mx/assets/img/social/lg_fb.svg" loading="lazy" alt="Facebook @meisaequip" title="Facebook @meisaequip" widht="100" height="100"  />
+            </a>
+            <a href="https://www.instagram.com/meisa_mex/" title="Instagram" target="_blank">
+               <img src="https://meisamex.com.mx/assets/img/social/lg_ig.svg" loading="lazy" alt="Instagram @meisa_mex" title="Instagram @meisa_mex" widht="100" height="100"  />
+            </a>
+            <a href="https://twitter.com/meisamex" title="Twitter" target="_blank">
+               <img src="https://meisamex.com.mx/assets/img/social/lg_tw.svg" loading="lazy"  alt="Twitter @meisa_mex" title="Twitter @meisa_mex" widht="100" height="100"  />
+            </a>
+            <a href="https://www.youtube.com/user/meisaequipos" title="Youtube" target="_blank">
+               <img src="https://meisamex.com.mx/assets/img/social/lg_you.svg" loading="lazy"  alt="Youtube @meisa_mex" title="Youtube @meisa_mex" widht="100" height="100"  />
+            </a>
+            <a href="https://www.linkedin.com/in/meisamex/" title="Linkedin" target="_blank">
+               <img src="https://meisamex.com.mx/assets/img/social/lg_lin.svg" loading="lazy"  alt="Linkedin @meisa_mex" title="Linkedin @meisa_mex" widht="100" height="100"  />
+            </a>
+       
+         </div>
+   </div>
+   <div class="li__footer ">
+      <iframe title="Ubicación de Meisa MX" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3765.560208665229!2d-99.56701140850782!3d19.30148236724018!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd8abd8654e1a1%3A0xc0dbdbabbf1a84bf!2sSegunda%205%20de%20Mayo%2020%2C%20Reforma%2C%20Delegaci%C3%B3n%20Santa%20Mar%C3%ADa%20Totoltepec%2C%2052100%20Toluca%20de%20Lerdo%2C%20M%C3%A9x.!5e0!3m2!1ses!2smx!4v1658210888350!5m2!1ses!2smx" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+   </div>
+   <div class="li__footer padd__esp">
+      <a href="https://meisamex.com.mx/">Home</a>
+      <a href="https://meisamex.com.mx/Inventario/">Equipment Inventory</a>
+      <a href="https://meisamex.com.mx/Maniobra/">Maneuver</a>
+      <a href="https://meisamex.com.mx/RentadeEquipos/">Equipment Rental</a>
+      <a href="https://meisamex.com.mx/PlantasdeLuz/">Light plants</a>
+      <a href="https://meisamex.com.mx/Contacto/">Contact</a>
+   </div>
