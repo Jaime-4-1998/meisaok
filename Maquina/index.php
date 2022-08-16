a0dd6b14fff1dcaf6de227a07501c845e44d7936
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2022-08-15 23:59:42 -0500

    el front de los prductos

--- Maquina/index.php
+++ Maquina/index.php
@@ -8,7 +8,7 @@
       <meta name="theme-color" content="#bf1520"/>
       <meta name="description" content="Meisa ofrece un amplio catalogo de productos de maquinaria, equipo y accesorios de cualquier ramo de la industria" />
       <title>Meisa - Contacto</title>
-      <link rel="preload" href="https://meisamex.com.mx/assets/css/style.css" as="style" onload="this.rel='stylesheet'">
+      <link rel="preload" href="http://localhost/meisa/assets/css/style.css" as="style" onload="this.rel='stylesheet'">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
       <link rel="stylesheet" href="http://localhost/meisa/assets/css/inve.css">
       <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -103,90 +103,130 @@
             {
             extract($row);
         ?>
-        <p class="card-text"> <i class="fas fa-thumbtack iconos"></i><?php echo str_replace("-", " ", $row['inve_nombre']); ?></p>
-        <p class="card-text"> <i class="fas fa-thumbtack iconos"></i> description: <?php echo $inve_desc; ?></p>
-                <?php 
-                    if($inve_youtube == '0'){
-                        echo ' ';
-                    }else{
-                        $output = '<div class="frame__youtube">'. $row['inve_youtube'] .'</div>'; echo $output;
-                    }
-                ?>
-        <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
-        <div class="swiper-wrapper">
-        <div class="swiper-slide img__inveget">
-          <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_img; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-        <div class="swiper-slide img__inveget">
-          <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgtrasera; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-        <div class="swiper-slide img__inveget">
-          <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldderecho; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-        <div class="swiper-slide img__inveget">
-          <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldizq; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-      </div>
-      <div class="swiper-button-next"></div>
-      <div class="swiper-button-prev"></div>
-    </div>
-    <div thumbsSlider="" class="swiper mySwiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_img; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-        <div class="swiper-slide">
-          <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgtrasera; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-        <div class="swiper-slide">
-          <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldderecho; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-        <div class="swiper-slide">
-          <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldizq; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
-        </div>
-      </div>
-    </div>
-        <?php } } else { ?>
+        <section>
+         <div class="product__meisa">
+            <h1><?php echo str_replace("-", " ", $row['inve_nombre']); ?></h1>
+            <h2><?php echo $inve_desc; ?></h2>
+         </div>
+        </section>
+        <section>
+          <div class="product_des_miesa">
+                    <!-- Swiper JS -->
+                           <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
+                              <div class="swiper-wrapper">
+                              <div class="swiper-slide img__inveget">
+                                 <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_img; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              <div class="swiper-slide img__inveget">
+                                 <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgtrasera; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              <div class="swiper-slide img__inveget">
+                                 <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldderecho; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              <div class="swiper-slide img__inveget">
+                                 <img data-enlargable src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldizq; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              </div>
+                              <div class="swiper-button-next"></div>
+                              <div class="swiper-button-prev"></div>
+                           </div>
+                           <div thumbsSlider="" class="swiper mySwiper">
+                              <div class="swiper-wrapper">
+                              <div class="swiper-slide">
+                                 <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_img; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              <div class="swiper-slide">
+                                 <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgtrasera; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              <div class="swiper-slide">
+                                 <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldderecho; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              <div class="swiper-slide">
+                                 <img src="http://localhost/meisa/meisaback/adminmeisa/assets/<?php echo $inve_imgldizq; ?>" loading="lazy" alt="<?php echo $inve_nombre; ?>" title="<?php echo $inve_nombre; ?>" />
+                              </div>
+                              </div>
+                           </div>
+                           <div class="product_esp">
+                              <h3>Especificaciones</h3>
+                              <p>Categoria: <?php echo $inve_catego; ?></p>
+                              <p>Marca: <?php echo $inve_marca; ?></p>
+                              <p>Modelo: <?php echo $inve_modelo; ?></p>
+                              <p># Serie: <?php echo $inve_serie; ?></p>
+                              <p>Año: <?php echo $inve_year; ?></p>
+                              <p>Corriente: <?php echo $inve_corriente; ?></p>
+                              <p>Motor: <?php echo $inve_motor; ?></p>
+                              <p>Capacidad: <?php echo $inve_capacidad; ?></p>
+                              <?php 
+                                 $number = number_format($inve_precio,2,'.',',');
+                              ?>
+                              <p>Precio: $<?php  echo $number; ?></p>
+                           </div>
+                     <!-- Swiper JS -->
+          </div>
+         <div class="form__pro">
+            <div>
+               <?php 
+                     if($inve_youtube == '0'){
+                           echo ' ';
+                     }else{
+                           $output = '<div class="frame__youtube">'. $row['inve_youtube'] .'</div>'; echo $output;
+                     }
+               ?>
+            </div>
+            <div>
+               <p>formulario</p>
+            </div>
+         </div>
+          <?php } } else { ?>
             Datos no encontrados ... 
-        <?php } ?>
-    <!-- Swiper JS -->
+          <?php } ?>
+        </section>
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
     <!-- Initialize Swiper -->
     <script src="http://localhost/meisa/assets/js/swipinve.js"></script>
     <footer>
-        <div class="footer__first">
-            <div class="footer__hr">
-                <img src="https://meisamex.com.mx/assets/img/art/clocnew.svg" loading="lazy" alt="Horarios de Meisa MX" title="Horarios de Meisa MX" width="28" height="100">
-                <p>Lunes a viernes de 9:00am - 18:00pm</p>
-            </div>
-            <p>Sábados de 9:00am - 14:00pm</p>
-        </div>
-        <div class="footer__second">
-        <a href="tel:+527222117664">(+52) 722-211-7694</a>
-        <a href="tel:+5215535688727">(+521) 55-3568-8727</a>
-        <a href="mailto:contacto@meisamex.com.mx">contacto@meisamex.com.mx</a>
-        </div>
-        <div class="footer__four">
-        <div class="footer__social__line movil__footer">
-            <a href="https://www.facebook.com/meisaequip" title="Facebook" target="_blank">
-            <img src="https://meisamex.com.mx/assets/img/social/lg_fb.svg" loading="lazy" alt="Facebook @meisaequip" title="Facebook @meisaequip" widht="100" height="100"  />
+        <div class="li__footer">
+         <div class="logo__foter">
+            <img src="https://meisamex.com.mx/assets/img/lg/9800mei039087053.svg" alt="Meisa MEX" title="Meisa MEX" width="80" height="100" />
+         </div>
+         <p>
+         <img src="https://meisamex.com.mx/assets/img/art/icon-location.svg" alt="">Segunda 5 de Mayo 20
+            Reforma, Delegación Santa María Totoltepec
+            52100 Toluca de Lerdo, Méx. 
+         </p>
+         <a href="tel:+527222117664"> (+52) 722-211-7694</a>
+         <a href="tel:+5215535688727"> (+521) 55-3568-8727</a>
+         <a href="mailto:contacto@meisamex.com.mx">contacto@meisamex.com.mx</a>
+         <div class="footer__social">
+            <a href="https://www.facebook.com/meisaequip" target="_blank" rel="noopener" aria-label="Facebook" title="Facebook">
+             <img src="https://meisamex.com.mx/assets/img/social/lg_fb.svg" loading="lazy" alt="Facebook @meisaequip" title="Facebook @meisaequip" widht="100" height="100"  />
             </a>
             <a href="https://www.instagram.com/meisa_mex/" title="Instagram" target="_blank">
-            <img src="https://meisamex.com.mx/assets/img/social/lg_ig.svg" loading="lazy" alt="Instagram @meisa_mex" title="Instagram @meisa_mex" widht="100" height="100"  />
+               <img src="https://meisamex.com.mx/assets/img/social/lg_ig.svg" loading="lazy" alt="Instagram @meisa_mex" title="Instagram @meisa_mex" widht="100" height="100"  />
             </a>
-        </div>
-        <div class="footer__social__line movil__footer">
             <a href="https://twitter.com/meisamex" title="Twitter" target="_blank">
-            <img src="https://meisamex.com.mx/assets/img/social/lg_tw.svg" loading="lazy"  alt="Twitter @meisa_mex" title="Twitter @meisa_mex" widht="100" height="100"  />
+               <img src="https://meisamex.com.mx/assets/img/social/lg_tw.svg" loading="lazy"  alt="Twitter @meisa_mex" title="Twitter @meisa_mex" widht="100" height="100"  />
             </a>
             <a href="https://www.youtube.com/user/meisaequipos" title="Youtube" target="_blank">
-            <img src="https://meisamex.com.mx/assets/img/social/lg_you.svg" loading="lazy"  alt="Youtube @meisa_mex" title="Youtube @meisa_mex" widht="100" height="100"  />
+               <img src="https://meisamex.com.mx/assets/img/social/lg_you.svg" loading="lazy"  alt="Youtube @meisa_mex" title="Youtube @meisa_mex" widht="100" height="100"  />
             </a>
             <a href="https://www.linkedin.com/in/meisamex/" title="Linkedin" target="_blank">
-            <img src="https://meisamex.com.mx/assets/img/social/lg_lin.svg" loading="lazy"  alt="Linkedin @meisa_mex" title="Linkedin @meisa_mex" widht="100" height="100"  />
+               <img src="https://meisamex.com.mx/assets/img/social/lg_lin.svg" loading="lazy"  alt="Linkedin @meisa_mex" title="Linkedin @meisa_mex" widht="100" height="100"  />
             </a>
-        </div>
-        </div>
+       
+         </div>
+      </div>
+      <div class="li__footer ">
+         <iframe title="Ubicación de Meisa MX" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3765.560208665229!2d-99.56701140850782!3d19.30148236724018!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd8abd8654e1a1%3A0xc0dbdbabbf1a84bf!2sSegunda%205%20de%20Mayo%2020%2C%20Reforma%2C%20Delegaci%C3%B3n%20Santa%20Mar%C3%ADa%20Totoltepec%2C%2052100%20Toluca%20de%20Lerdo%2C%20M%C3%A9x.!5e0!3m2!1ses!2smx!4v1658210888350!5m2!1ses!2smx" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div class="li__footer padd__esp">
+         <a href="https://meisamex.com.mx/">Inicio</a>
+         <a href="https://meisamex.com.mx/Inventario/">Inventario de Equipo</a>
+         <a href="https://meisamex.com.mx/Maniobra/">Maniobra</a>
+         <a href="https://meisamex.com.mx/RentadeEquipos/">Renta de Equipos</a>
+         <a href="https://meisamex.com.mx/PlantasdeLuz/">Plantas de Luz</a>
+         <a href="https://meisamex.com.mx/Contacto/">Contacto</a>
+      </div>    
     </footer>
     <script src="https://meisamex.com.mx/assets/js/index.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
